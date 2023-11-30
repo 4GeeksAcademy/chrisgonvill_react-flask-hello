660fc72d1c5d1e20fcf5381df19656415dcfc694
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-11-30 22:39:53 +0000

    workingOnLoginRoute

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,13 +1,20 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, current_app
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 import json
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+# from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
+
+# api.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+# jwt = JWTManager(api)
 
 # Allow CORS requests to this API
 CORS(api)
@@ -44,4 +51,18 @@ def create_one_user():
         "password" : body["password"],
         "is_admin" : body["is_admin"]
     }
+
     return jsonify({"msg": "user created succesfull", "user_added": user_ok }), 200
+
+
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
